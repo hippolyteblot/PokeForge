2bbd2f30cc3ee45f111a3d07a915418fe50f4da5
Hippo <hippolytebl@gmail.com>
2023-03-01 21:36:56 +0100

    Binding of the types with sprites

--- app/src/main/java/com/example/pokeforge/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/pokeforge/ui/home/HomeFragment.kt
@@ -13,6 +13,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.pokeforge.MainActivity
 import com.example.pokeforge.Pokemon
 import com.example.pokeforge.PokemonAdapter
+import com.example.pokeforge.PokemonType
 import com.example.pokeforge.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
@@ -44,7 +45,7 @@ class HomeFragment : Fragment() {
         pokemons = ArrayList<Pokemon>()
         pokemons.add(Pokemon("Bulbasaur", 1, listOf(), 7, 69, 64, listOf(45, 49, 49, 65, 65, 45), 5, listOf(9, 26)))
         pokemons.add(Pokemon("Ivysaur", 2, listOf(), 16, 130, 142, listOf(60, 62, 63, 80, 80, 60), 5, listOf(6, 9)))
-        pokemons.add(Pokemon("Charmeon", 5, listOf(), 16, 142, 142, listOf(58, 64, 58, 80, 65, 80), 5, listOf(9, 6)))
+        pokemons.add(Pokemon("Charmeon", 5, listOf(PokemonType.WATER, PokemonType.FIRE), 16, 142, 142, listOf(58, 64, 58, 80, 65, 80), 5, listOf(9, 6)))
         pokemons.add(Pokemon("Charizard", 6, listOf(), 36, 240, 240, listOf(78, 84, 78, 109, 85, 100), 5, listOf(1, 5)))
         pokemons.add(Pokemon("Squirtle", 7, listOf(), 5, 88, 64, listOf(44, 48, 65, 50, 64, 43), 5, listOf(132, 133)))
         pokemons.add(Pokemon("Wartortle", 8, listOf(), 16, 155, 142, listOf(59, 63, 80, 65, 80, 58), 5, listOf(133, 76)))
