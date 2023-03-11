9182d4818c0e454e2fb817ce6d3fb01a58d6db22
p2109548 <noah.heraud@etu-lyon1.fr>
2023-03-11 21:49:13 +0100

    api stat binding working

--- app/src/main/java/com/example/pokeforge/PokemonViewerActivity.kt
+++ app/src/main/java/com/example/pokeforge/PokemonViewerActivity.kt
@@ -1,12 +1,17 @@
 package com.example.pokeforge
 
 import android.annotation.SuppressLint
+import android.content.ContentValues.TAG
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.example.pokeforge.databinding.ActivityPokemonViewerBinding
+import com.google.firebase.FirebaseApp
+import com.google.firebase.database.ktx.database
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import kotlinx.coroutines.*
 import java.util.*
 import kotlin.collections.ArrayList
@@ -24,7 +29,21 @@ class PokemonViewerActivity : AppCompatActivity() {
         setContentView(binding.root)
         pokemon = intent.getSerializableExtra("pokemon") as Pokemon
 
-
+        FirebaseApp.initializeApp(this)
+        val db = Firebase.firestore
+        val user = hashMapOf(
+            "first" to "Ada",
+            "last" to "Lovelace",
+            "born" to 1815
+        )
+        db.collection("users")
+            .add(user)
+            .addOnSuccessListener { documentReference ->
+                Log.d(TAG, "DocumentSnapshot added with ID: ${documentReference.id}")
+            }
+            .addOnFailureListener { e ->
+                Log.w(TAG, "Error adding document", e)
+            }
 
 
 
