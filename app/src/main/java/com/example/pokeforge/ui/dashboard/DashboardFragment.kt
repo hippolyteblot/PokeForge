0b3c5dfcc3845f20fc34dccfbd1609faf7dd99cd
Noé Chouteau <osolarix@gmail.com>
2023-03-13 09:22:56 +0100

    pour pull

--- app/src/main/java/com/example/pokeforge/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/example/pokeforge/ui/dashboard/DashboardFragment.kt
@@ -1,21 +1,28 @@
 package com.example.pokeforge.ui.dashboard
 
 import android.app.AlertDialog
+import android.app.Dialog
 import android.content.DialogInterface
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.pokeforge.APISpritesClient
 import com.example.pokeforge.MainActivity
 import com.example.pokeforge.R
+import com.example.pokeforge.SpriteSelectionAdapter
 import com.example.pokeforge.databinding.FragmentDashboardBinding
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+
 
 class DashboardFragment : Fragment() {
 
@@ -47,6 +54,31 @@ class DashboardFragment : Fragment() {
             builder.setPositiveButton("Oui", DialogInterface.OnClickListener { dialog, which ->
                 Toast.makeText(activity, "Oeuf ajouté !", Toast.LENGTH_SHORT).show()
                 binding.textViewMoney.text = (binding.textViewMoney.text.toString().toInt() - 1000).toString()
+
+                var poke = ""
+                val db = Firebase.firestore
+                val UID = (activity as MainActivity).userUID
+                val collectionRef = db.collection("pokemon_available")
+
+                collectionRef.whereEqualTo("isMythical", true)
+                    .get()
+                    .addOnSuccessListener { documents ->
+                        collectionRef.whereEqualTo("isLegendary", true)
+                            .get()
+                            .addOnSuccessListener { documents2 ->
+                                val listeLegend = documents.documents
+                                val listeMyth = documents2.documents
+                                val liste = listeLegend + listeMyth
+                                val taille = liste.size
+                                val random = (0 until taille).random()
+                                val pokemon = liste[random]
+                                println("DocumentSnapshot data: ${pokemon.data}")
+                                poke = pokemon.data?.get("name").toString()
+                            }
+                    }
+                    .addOnFailureListener { exception ->
+                        Log.w("TAG", "Error getting documents: ", exception)
+                    }
 
             })
             builder.setNegativeButton("Non", DialogInterface.OnClickListener { dialog, which ->
